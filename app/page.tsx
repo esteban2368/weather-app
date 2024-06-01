6095289ec0af117f6cd290ff20aa03e6433865a1
esteban.d2168 <esteban.d2168@gmail.com>
2024-06-01 04:33:48 +0000

    sass: create partials components like button, other utilities, grid and colors classes

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,20 @@ import styles from "@/styles/HomePage.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <section>
-          <div>
-            <button role="button">
+    <main className={`${styles.main} container`}>
+        <section className="row">
+          <div className="column-10">
+            <button role="button" className="button button-gray">
               Search for places
             </button>
+          </div>
+          <div className="column-2">
             <button role="button">
               locarion
             </button>
-            <span className="heading-5">Today's Hightlights</span>
           </div>
           <div>
+            <span className="heading-4">Today's Hightlights</span>
             <Image
               alt="Shower"
               src="/Shower.png"
